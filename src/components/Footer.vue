0ec557047b9b1964c9cd068a968b021003d91567
gangsthub <paulmelero@gmail.com>
2021-08-20 19:43:53 +0200

    first commit :shipit:

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { isDark, toggleDark } from '~/logic'
+</script>
+
+<template>
+  <nav class="text-xl mt-6">
+    <button class="cursor-pointer mx-2 !outline-none" @click="toggleDark">
+      <carbon-moon v-if="isDark" />
+      <carbon-sun v-else />
+    </button>
+  </nav>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div>
+    <h1>Mama Dulce</h1>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
